ae4bbeef1dc3b606486b3f41b82058f08f197d67
Konstantin <ostapx79gmail.com>
2025-03-28 22:55:54 +0300

    первая программа на Котлине, и комментарий в java и котлин

---
+++ src/commentsLesson/CommentsForKotlinJava.kt
@@ -0,0 +1,13 @@
+package commentsLesson
+
+fun main() {
+    // Однострочный комментарий
+    // One-line comments
+    println("My name is Konstantin")
+
+    /*
+    Многострочный комментарий
+    Multi-line comment
+     */
+
+}
