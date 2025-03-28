54bf18d487d1c032815aa3b2d582db1c32282419
Konstantin <ostapx79gmail.com>
2025-03-28 22:55:54 +0300

    syntax в Java

---
+++ src/commentsLesson/CommentsForJavaKotlin.java
@@ -0,0 +1,19 @@
+package commentsLesson;
+
+/**
+ * Документирующие комментарии
+ * Documenting comments
+ */
+
+public class CommentsForJavaKotlin {
+    public static void main(String[] args) {
+        // Однострочный комментарий
+        // One-line comments
+        System.out.println("My name is Konstantin");
+
+        /*
+        Многострочный комментарий
+        Multi-line comment
+         */
+    }
+}
---
+++ src/syntax/SyntaxJavaKotlin.java
@@ -0,0 +1,7 @@
+package syntax;
+
+public class SyntaxJavaKotlin {
+    public static void main(String[] args) {
+        System.out.println("My name is Java");
+    }
+}
