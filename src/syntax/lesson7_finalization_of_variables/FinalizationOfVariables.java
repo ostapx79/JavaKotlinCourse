31476871b9974bf821b21e0d2acc9674931091d3
Konstantin <ostapx79gmail.com>
2025-03-30 23:25:18 +0300

    7 Финализация переменных [Java Kotlin с нуля]

---
+++ src/syntax/lesson7_finalization_of_variables/FinalizationOfVariables.java
@@ -0,0 +1,14 @@
+package syntax.lesson7_finalization_of_variables;
+
+public class FinalizationOfVariables {
+    public static void main(String[] args) {
+        final int firstInt = 4;
+        final int secondInt = 5;
+//        secondInt = firstInt + secondInt;
+        System.out.println("firstInt: " + firstInt + ", secondInt: " + secondInt);
+
+        final var nameOfBoss = "Konstantin Galus";
+//        nameOfBoss = "Anatoli Galus";
+        System.out.println("nameOfBoss: " + nameOfBoss);
+    }
+}
