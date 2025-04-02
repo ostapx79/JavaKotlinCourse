8ee41b3223b7edddce3b18071fa81747e6b79a29
Konstantin <ostapx79gmail.com>
2025-04-02 23:56:25 +0300

    логическое and и or в java/kotlin

---
+++ src/syntax/lesson9_logical_and_or/LogicalAndOr.java
@@ -0,0 +1,26 @@
+package syntax.lesson9_logical_and_or;
+
+public class LogicalAndOr {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+
+        // операций сравнения
+        boolean isEqual = a == b;
+        boolean isNotEqual = a != b;
+        boolean isGreater = a > b;
+        boolean isGreaterOrEqual = a >= b;
+        boolean isLess = a < b;
+        boolean isLessOrEqual = a >= b;
+
+        // And(&&) Or(||)
+        boolean aIsGreaterThanB = a > b;
+        System.out.println(aIsGreaterThanB);
+        boolean aIsEqualToB = a == b;
+        boolean logicalAND = aIsGreaterThanB && aIsEqualToB;
+        boolean logicalOR = aIsGreaterThanB || aIsEqualToB;
+        System.out.println("The result of expression: a > b AND a == b is " + logicalAND);
+        System.out.println("The result of expression: a > b AND a == b is " + logicalOR);
+
+
+    }
+}
