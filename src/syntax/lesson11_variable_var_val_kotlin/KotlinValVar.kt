92c935e96331d21b0efce027056151bbf5f0bff4
Konstantin <ostapx79gmail.com>
2025-04-03 23:41:11 +0300

    переменные в kotlin

---
+++ src/syntax/lesson11_variable_var_val_kotlin/KotlinValVar.kt
@@ -0,0 +1,9 @@
+package syntax.lesson11_variable_var_val_kotlin
+
+fun main() {
+    var intNumber: Int = 5
+    var someString: String = "My name is Konstantin"
+
+    intNumber = 8 // var изменяемая переменная
+    val nameOfManager = "Tolea" // val не изменяемая переменная
+}
