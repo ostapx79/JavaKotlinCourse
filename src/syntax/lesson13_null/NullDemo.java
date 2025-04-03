416b42e1237862c952449aa36fbbc9b72bc44375
Konstantin <ostapx79gmail.com>
2025-04-04 00:18:09 +0300

    null в java и kotlin

---
+++ src/syntax/lesson13_null/NullDemo.java
@@ -0,0 +1,10 @@
+package syntax.lesson13_null;
+
+public class NullDemo {
+    public static void main(String[] args) {
+        String nullableString = null;
+        System.out.println(nullableString);
+        Integer nullableInt = null;
+        System.out.println(nullableInt);
+    }
+}
