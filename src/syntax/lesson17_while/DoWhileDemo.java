9e89f659b7995ceeeee064eb3cfb583210686bcd
Konstantin <ostapx79gmail.com>
2025-04-05 01:01:10 +0300

    циклы while и do-while в Java/Kotlin

---
+++ src/syntax/lesson17_while/DoWhileDemo.java
@@ -0,0 +1,29 @@
+package syntax.lesson17_while;
+
+import java.util.Scanner;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+
+        var count = 0;
+
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter your age: ");
+            int age = scanner.nextInt();
+
+            if (age >= 18) {
+                System.out.println("You are an adult.");
+            } else if (age >= 13) {
+                System.out.println("You are an teenager.");
+            } else {
+                System.out.println("You are an infant.");
+            }
+            count++;
+        } while (count < 4);
+
+        System.out.println("Exit from while-cycle");
+
+//        scanner.close();
+    }
+}
