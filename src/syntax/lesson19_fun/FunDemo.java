d7973a95effc75f85754b31f7dd4f967835006a1
Konstantin <ostapx79gmail.com>
2025-04-06 00:22:16 +0300

    функций в Java/Kotlin

---
+++ src/syntax/lesson19_fun/FunDemo.java
@@ -0,0 +1,13 @@
+package syntax.lesson19_fun;
+
+public class FunDemo {
+    public static void main(String[] args) {
+        System.out.println("The sum of numbers 15 and 35 is " + sumOfTwoNumbers(15, 35));
+        System.out.println("The sum of numbers 37 and 23 is " + sumOfTwoNumbers(37, 23));
+        System.out.println("The sum of numbers 5 and 35 is " + sumOfTwoNumbers(5, 35));
+    }
+
+    private static long sumOfTwoNumbers(int a, int b) {
+        return a + b;
+    }
+}
