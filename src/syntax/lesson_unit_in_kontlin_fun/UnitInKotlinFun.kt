ce0244a429f3856e88342cc4dce42b9d9588337d
Konstantin <ostapx79gmail.com>
2025-04-07 23:16:37 +0300

    лишняя точка с запетой, убрал точку с запетой

--- src/syntax/lesson_unit_in_kontlin_fun/UnitInKotlinFun.kt
+++ src/syntax/lesson_unit_in_kontlin_fun/UnitInKotlinFun.kt
@@ -4,8 +4,8 @@ fun main() {
 
     // анонимная функция (лямбда выражение), которая ни чего не возвращает
     val multiply:(Int, Int) -> Unit = { a, b -> println("a * b = ${a*b}") }
-    multiply(22, 3);
-    multiply1(9, 3);
+    multiply(22, 3)
+    multiply1(9, 3)
     val sum: String = multiply2(22, 3)
     println(sum)
 }
