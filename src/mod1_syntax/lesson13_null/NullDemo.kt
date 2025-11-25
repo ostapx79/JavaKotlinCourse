b24f7ea3ae367d1da470e65ee093442019e69a61
Konstantin <ostapx79gmail.com>
2025-11-25 22:23:52 +0300

    изменен файл NullDemo.kt

--- src/mod1_syntax/lesson13_null/NullDemo.kt
+++ src/mod1_syntax/lesson13_null/NullDemo.kt
@@ -4,7 +4,7 @@ fun main() {
     var nullableString: String? = "Konstantin"
     println(nullableString?.uppercase() ?: "Null value here")
 
-    val PI:Double? = null
+    val PI: Double? = null
     println("The pi constant is ${PI ?: "3.14"}")
 
 }
