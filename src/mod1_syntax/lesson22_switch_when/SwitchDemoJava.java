6a63966db160086b5866432232c60dfd33abee84
Konstantin <ostapx79gmail.com>
2025-11-27 02:01:43 +0300

    добавел файл SwitchDemoJava.java

---
+++ src/mod1_syntax/lesson22_switch_when/SwitchDemoJava.java
@@ -0,0 +1,25 @@
+package mod1_syntax.lesson22_switch_when;
+
+public class SwitchDemoJava {
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 4;
+        int num3 = 2;
+
+        final int ZERO_NUMBER = 3;
+        final int FIRST_NUMBER = 2;
+        final int SECOND_NUMBER = 4;
+
+        final int expression = Math.max(Math.max(ZERO_NUMBER, FIRST_NUMBER), SECOND_NUMBER);
+        String str;
+
+        switch (expression) {
+            case ZERO_NUMBER -> str = "abc";
+            case FIRST_NUMBER -> str = "def";
+            case SECOND_NUMBER -> str = "ghi";
+            default -> str = "jkl";
+        }
+        System.out.println(str);
+    }
+}
