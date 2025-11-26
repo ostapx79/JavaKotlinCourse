39d4bc836b7264416aba9d3f38f4297bba25ece4
Konstantin <ostapx79gmail.com>
2025-11-26 23:02:33 +0300

    изменен файл ForDemo.java

--- src/mod1_syntax/lesson18_for/ForDemo.java
+++ src/mod1_syntax/lesson18_for/ForDemo.java
@@ -4,7 +4,7 @@ public class ForDemo {
     public static void main(String[] args) {
 
         int sum = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i <= 10; i++) {
             sum += i;
             System.out.print(i + " ");
         }
