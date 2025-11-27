450c57a9da2410e809f97d0312bdb5456a604c1d
Konstantin <ostapx79gmail.com>
2025-11-27 23:11:12 +0300

    изменен файл SwitchDemoJava.java

--- src/mod1_syntax/lesson22_switch_when/SwitchDemoJava.java
+++ src/mod1_syntax/lesson22_switch_when/SwitchDemoJava.java
@@ -8,18 +8,24 @@ public class SwitchDemoJava {
         int num3 = 2;
 
         final int ZERO_NUMBER = 3;
-        final int FIRST_NUMBER = 2;
+        final int FIRST_NUMBER = 8;
         final int SECOND_NUMBER = 4;
 
         final int expression = Math.max(Math.max(ZERO_NUMBER, FIRST_NUMBER), SECOND_NUMBER);
-        String str;
+//        String str
+//        switch(expression) {
+//            case ZERO_NUMBER -> str = "abc";
+//            case FIRST_NUMBER -> str = "def";
+//            case SECOND_NUMBER -> str = "ghi";
+//            default -> str = "jkl";
+//        }
 
-        switch (expression) {
-            case ZERO_NUMBER -> str = "abc";
-            case FIRST_NUMBER -> str = "def";
-            case SECOND_NUMBER -> str = "ghi";
-            default -> str = "jkl";
-        }
+        String str = switch (expression) {
+            case ZERO_NUMBER -> "abc";
+            case FIRST_NUMBER -> "def";
+            case SECOND_NUMBER -> "ghi";
+            default -> "jkl";
+        };
         System.out.println(str);
     }
 }
