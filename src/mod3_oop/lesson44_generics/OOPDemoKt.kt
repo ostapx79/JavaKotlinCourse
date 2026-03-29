93f7daac0d3da1ea2d92348d8a7778d5207c510b
Constantin Galus <ostapx79@gmail.com>
2026-03-29 17:14:17 +0300

    проходим урок 44 Generics [ Java и Kotlin]

--- src/mod3_oop/lesson44_generics/OOPDemoKt.kt
+++ src/mod3_oop/lesson44_generics/OOPDemoKt.kt
@@ -1,5 +1,11 @@
 package mod3_oop.lesson44_generics
 
 fun main() {
+    val person = PersonKt("Bob", 1)
+    println(person)
+    person.age = 23
+    println(person)
+    person.sayHello()
+    println(person.id)
 
 }
--- src/mod3_oop/lesson44_generics/PersonKt.kt
+++ src/mod3_oop/lesson44_generics/PersonKt.kt
@@ -3,11 +3,20 @@ package mod3_oop.lesson44_generics
 import java.text.SimpleDateFormat
 import java.util.Date
 
-open class PersonKt(var name: String, var age: Int) {
+data class PersonKt<T>(
+    var name: String,
+    var age: Int,
+    var id: T?
+) {
 
-    constructor() : this("", 0)
-
-    constructor(name: String) : this(name, 0) { this.name = name }
+    constructor() : this("", 0, null)
+    constructor(name: String) : this(name, 0, null) {
+        this.name = name
+    }
+    constructor(name: String, id: T) : this(name, 0, null) {
+        this.name = name
+        this.id = id
+    }
 
     var birth: Date = Date()
         get() = SimpleDateFormat("yyyy.MM.dd").parse(SimpleDateFormat("yyyy.MM.dd").format(field))
@@ -15,20 +24,8 @@ open class PersonKt(var name: String, var age: Int) {
             field = value
         }
 
-    init {
-        println("The new object of kotlin class PersonKt is created!")
-    }
-
-    open fun sayHello(): Unit {
+    fun sayHello(): Unit {
         println("Hello, my name is ${this.name}.")
     }
 
-    companion object {
-        const val KIND = "Human"
-
-        fun getInstance() = PersonKt()
-        fun getInstance(name: String) = PersonKt(name)
-        fun getInstance(name: String, age: Int) = PersonKt(name, age)
-        fun getInstance(other: PersonKt) = PersonKt(other.name, other.age)
-    }
 }
